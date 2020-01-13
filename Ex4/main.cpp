53cdff52db3e2d085c6a7b338c5791b0c5d4949c
Aslakjs <aslakjs@stud.ntnu.no>
2020-01-13 15:08:35 +0100

    started on assignment 4

---
+++ Ex4/main.cpp
@@ -0,0 +1,13 @@
+/*----------------------------------------------------
+TITLE:  Assignment 4
+COURSE: TDT4102 - Programming
+AUTHOR: Aslak J. Strand (c)
+
+Notes:
+    - N/A
+---------------------------------------------------*/
+
+
+int main(void) {
+    return 0;
+}
