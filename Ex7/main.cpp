b9a486c548c7aa017c43de6f606748fc97fadd5d
Aslakjs <aslakjs@stud.ntnu.no>
2020-02-10 19:27:28 +0100

    Ready for assignment 7

---
+++ Ex7/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------
+TITLE:  Assignment 7
+COURSE: TDT4102 - Programming
+AUTHOR: Aslak J. Strand (c)
+
+Notes:
+    - N/A
+---------------------------------------------------*/
+
+#include <iostream>
+#include "std_lib_facilities.h"
+using namespace std;
+
+
+
+int main()
+{
+	cout << "Hello, World!\n";
+
+	keep_window_open();
+}
